3970abd08eabda3f5ca376b8db61339337f78664
Avinash Pandey <avinash.12009084@lpu.in>
2024-03-11 11:59:39 +0530

    Cors and CookieParser

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// .use used for middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "20kb" })); // Parse incoming requests with JSON payloads
+
+// Encode urls extended used for give objects inside objects
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+
+// Static used for file storage
+app.use(express.static("public"));
+
+//cookieParser We can acces cookies of user and we can set it up
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 // import express from "express";
 import * as dotenv from "dotenv";
 import connectToDB from "./db/connectToDB.js";
+import { app } from "./app.js";
 
 dotenv.config();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 // const app = express();
 
 // app.get("/", (req, res) => {
 //   res.send("API is running!");
 // });
 
-connectToDB();
+connectToDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to the database: ", error);
+  });
